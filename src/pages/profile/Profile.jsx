a2ff1ba5442aeb942dedc0f03dbb9f5096ff14a8
Alef Santos <alef.santos@al.infnet.edu.br>
2025-11-27 18:50:56 -0300

    feat: update profile component to display user information and translate text to English

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,18 @@
 import style from "./Profile.module.css";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth/AuthContext";
 
 export default function Profile() {
+  const { user } = useContext(AuthContext);
+
   return (
     <main className={style.profile_container}>
-      <h2 className={style.profile_title}>Meu Perfil</h2>
+      <h2 className={style.profile_title}>My Profile</h2>
 
       <div className={style.profile_box}>
-        <p><strong>Nome:</strong> Usuário</p>
-        <p><strong>Email:</strong> usuario@email.com</p>
-        <p><strong>Status:</strong> Logado</p>
+        <p><strong>Name:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Status:</strong> Logged In</p>
       </div>
     </main>
   );
